417b6f0589ffaedc0bd525ed89e8b38b8d8db450
Stefan Engstrom <sengstrom@ena.com>
2017-05-26 17:41:15 -0500

    now completely different

--- configurator.go
+++ configurator.go
@@ -2,73 +2,273 @@ package consultant
 
 import (
 	"fmt"
+
 	"github.com/hashicorp/consul/watch"
-	"github.com/hashicorp/consul/api"
+)
+
+const (
+	updateChanLength = 100 // must exceed the maximum number of watch plans
+	chanLength       = 10  //
 )
 
 // A config object that implements this interface can be initialized from consul's
-// KV store or from it's service list and then automatically updated.
-// Set up init/watch plans for a KV prefix with `KVPrefixConfigurator()`.
-// A service can be similarly observed with `ServiceConfigurator()`.
-// It is up to the implementation to ensure that Updates() and use of the config object
-// are done in a thread-safe way.
-// See the test code for an example implementation.
+// KV store or from it's service list and then automatically updated as these things change in consul.
 type Configurator interface {
 	Update(uint64, interface{})
 }
 
-// KVPrefixConfigurator initializes the passed config object for updates under a given prefix
-// Current values are read from the consul KV store and stored in 'config' (passed by reference).
-// A watch plan is set up to call Update() whenever the KV prefix is changed.
-// Update() should expect to receive a KV pair list (github.com/hashicorp/consul/api: KVPairs)
-func (c *Client) KVPrefixConfigurator(config Configurator, prefix string) (*watch.Plan, error) {
+type serviceDetails struct {
+	plan        *watch.Plan
+	tag         string
+	passingOnly bool
+}
 
-	kvps, _, err := c.KV().List(prefix, nil)
-	if err != nil {
-		return nil, fmt.Errorf("Trouble getting the KVs under: %s", prefix)
+type update struct {
+	index uint64
+	data  interface{}
+}
+
+type ConfigChan chan Configurator
+
+// ConfigManager keeps a private copy of the Configurator object in order to manage thread-safe access.
+type ConfigManager struct {
+	client        *Client // a consultant client
+	config        Configurator // private copy
+	prefixPlans   map[string]*watch.Plan  // the prefixes we are managing
+	servicePlans  map[string]*serviceDetails // services we are managing
+	subscriptions map[*ConfigChan]bool // user can subscribe to updates
+	seedChan      ConfigChan
+	readChan      chan ConfigChan
+	updateChan    chan update
+	syncChan      chan chan chan bool
+	stopChan      chan bool
+}
+
+// NewConfigManager creates a new instance and kicks off a manager for it
+func (client *Client) NewConfigManager(config Configurator) *ConfigManager {
+
+	cm := &ConfigManager{
+		client:        client,
+		config:        config,
+		prefixPlans:   make(map[string]*watch.Plan),
+		servicePlans:  make(map[string]*serviceDetails),
+		subscriptions: make(map[*ConfigChan]bool),
+		seedChan:      make(ConfigChan, chanLength),
+		readChan:      make(chan ConfigChan, chanLength),
+		updateChan:    make(chan update, updateChanLength),
+		syncChan:      make(chan chan chan bool),
+		stopChan:      make(chan bool, 1),
 	}
-	config.Update(0, kvps)
 
-	wp, err := c.WatchKeyPrefix(prefix, true, config.Update)
+	// send in the config first so this is the first thing the handler sees
+	cm.Seed(config)
+
+	cm.configHandler()
+
+	return cm
+}
+
+// Seed replaces the config handled by cm and updates the consul-dependent information
+func (cm *ConfigManager) Seed(config Configurator) {
+	cm.seedChan <- config
+}
+
+// Read retrieves the current configuration
+func (cm *ConfigManager) Read() Configurator {
+	req := make(ConfigChan)
+	cm.readChan <- req
+	return <-req
+}
+
+func (cm *ConfigManager) configHandler() {
+	go func() { //  make sure the handler is running before we return
+	loop:
+		for {
+			select {
+
+			// initialize the config object with non-consul items
+			case seed := <-cm.seedChan:
+				//fmt.Println("seedChan")
+				cm.config = seed
+
+				// fill in information from consul
+				for prefix, _ := range cm.prefixPlans {
+					cm.updateKVPrefix(prefix)
+				}
+				for service, details := range cm.servicePlans {
+					cm.updateService(service, details.tag, details.passingOnly)
+				}
+
+			case req := <-cm.readChan:
+				//fmt.Println("readChan")
+				req <- cm.config
+
+			case u := <-cm.updateChan:
+				//fmt.Println("updateChan")
+				cm.config.Update(u.index, u.data) // user-defined handling
+				cm.handleSubscriptions()
+
+			case ch := <-cm.syncChan:
+				//fmt.Println("syncChan")
+				c1 := make(chan bool)
+				ch <- c1 // say: sync is yours
+				<-c1     // wait until ready to move on again
+
+			case <-cm.stopChan:
+				//fmt.Println("stopChan")
+				cm.cleanup()
+				break loop
+			}
+		}
+		fmt.Println("Exiting the handler")
+	}()
+}
+
+// transform a callback to a channel push
+func (cm *ConfigManager) updateHandler(index uint64, data interface{}) {
+	cm.updateChan <- update{
+		index: index,
+		data:  data,
+	}
+}
+
+func (cm *ConfigManager) Subscribe() *ConfigChan {
+	ch := make(ConfigChan, 1)
+	cm.subscriptions[&ch] = true
+	return &ch
+}
+
+func (cm *ConfigManager) Unsubscribe(ch *ConfigChan) {
+	_, ok := cm.subscriptions[ch]
+	if ok {
+		delete(cm.subscriptions, ch)
+	}
+}
+
+func (cm *ConfigManager) handleSubscriptions() {
+	for ch := range cm.subscriptions {
+		// Replace current item in the queue if there is something there
+		if len(*ch) == 1 {
+			<-*ch
+		}
+		*ch <- cm.config
+	}
+}
+
+// Stop shuts down the plans, channels, and the handler
+func (cm *ConfigManager) Stop() {
+	cm.stopChan <- true
+}
+
+// lock pauses the handler while we do some otherwise thread-unsafe stuff
+func (cm *ConfigManager) lock() chan bool {
+	c2 := make(chan chan bool)
+	cm.syncChan <- c2
+	return <-c2
+}
+
+// unlock tells the handler that it is okay to resume normal operations
+func unlock(sync chan bool) {
+	sync<-true
+}
+
+// AddKvPrefix starts watching the given prefix and updates the config with current values
+func (cm *ConfigManager) AddKVPrefix(prefix string) error {
+
+	var err error
+
+	sync := cm.lock()
+	defer unlock(sync)
+
+	wp, ok := cm.prefixPlans[prefix]
+	if ok {
+		wp.Stop()
+	}
+
+	wp, err = cm.client.WatchKeyPrefix(prefix, true, cm.updateHandler)
 	if err != nil {
-		return nil, fmt.Errorf("Trouble building the watch plan: %s", err)
+		return fmt.Errorf("Trouble building the watch plan: %s", err)
 	}
 
 	go func() {
-		err := wp.Run(c.config.Address)
+		err := wp.Run(cm.client.config.Address)
 		if err != nil {
 			fmt.Fprintf(wp.LogOutput, "Watch plan failed for prefix: %s", prefix)
 		}
 	}()
 
-	return wp, nil
+	cm.prefixPlans[prefix] = wp
+
+	return nil
 }
 
-// ConfigureServices initializes the passed config object for updates about a services.
-// A random passing service populate the config.
-// When anything changes for those services config.Update() is notified and config is updated.
-// Update() should expect to receive a service entry list (github.com/hashicorp/consul/api: []*ServiceEntry)
-func (c *Client) ServiceConfigurator(config Configurator, service string, tag string,
-	passingOnly bool, options *api.QueryOptions) (*watch.Plan, error) {
+// AddService starts watching the specified service and updates the config
+func (cm *ConfigManager) AddService(service, tag string, passingOnly bool) error {
 
-	seList, _, err := c.Health().Service(service, tag, passingOnly, nil)
-	if err != nil {
-		return nil, fmt.Errorf("Trouble finding a passing service for %s (tag=%s)",service,tag)
+	var err error
+
+	sync := cm.lock()
+	defer unlock(sync)
+
+	details, ok := cm.servicePlans[service]
+	if ok {
+		details.plan.Stop()
 	}
-	config.Update(0,seList)
 
-	wp, err := c.WatchService(service,tag, passingOnly, true, config.Update)
+	details = &serviceDetails{
+		tag:         tag,
+		passingOnly: passingOnly,
+	}
+
+	details.plan, err = cm.client.WatchService(service, tag, passingOnly, true, cm.updateHandler)
 	if err != nil {
-		return nil, fmt.Errorf("Trouble building the watch plan: %s", err)
+		return fmt.Errorf("Trouble building the watch plan: %s", err)
 	}
 
 	go func() {
-		err := wp.Run(c.config.Address)
+		err := details.plan.Run(cm.client.config.Address)
 		if err != nil {
-			fmt.Fprintf(wp.LogOutput, "Watch plan failed for service: %s", service)
+			fmt.Fprintf(details.plan.LogOutput, "Watch plan failed for service: %s", service)
 		}
 	}()
 
-	return wp, nil
+	cm.servicePlans[service] = details
 
+	return nil
+}
+
+// updateKVPrefix - list kv:s in the prefix and update our config with the result
+func (cm *ConfigManager) updateKVPrefix(prefix string) error {
+	kvps, _, err := cm.client.KV().List(prefix, nil)
+	if err != nil {
+		return fmt.Errorf("Trouble getting the KVs under: %s", prefix)
+	}
+	cm.updateHandler(0, kvps)
+
+	return nil
+}
+
+// updateService lists current services and forces an update
+func (cm *ConfigManager) updateService(service, tag string, passingOnly bool) error {
+	seList, _, err := cm.client.Health().Service(service, tag, passingOnly, nil)
+	if err != nil {
+		return fmt.Errorf("Trouble finding a passing service for %s (tag=%s)", service, tag)
+	}
+	cm.updateHandler(0, seList)
+
+	return nil
+}
+
+// cleanup frees up resources in the ConfigManager
+func (cm *ConfigManager) cleanup() {
+
+	// Shut down the watch plans
+	for _, details := range cm.servicePlans {
+		details.plan.Stop()
+	}
+
+	// Close subscriber channels
+	for ch, _ := range cm.subscriptions {
+		close(*ch)
+	}
 }
--- configurator_test.go
+++ configurator_test.go
@@ -5,14 +5,12 @@ package consultant_test
 import (
 	"fmt"
 	"strconv"
-	"sync"
 	"testing"
 	"time"
 	"net/http"
 
 	"github.com/hashicorp/consul/api"
 	"github.com/hashicorp/consul/testutil"
-	"github.com/hashicorp/consul/watch"
 	"github.com/myENA/consultant"
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
@@ -39,27 +37,6 @@ const (
 
 )
 
-func ExampleConfigurator() {
-	// minimal implementation
-	c := &config{}
-	consul,err := consultant.NewDefaultClient()
-	if err != nil {
-		panic("no consul")
-	}
-
-	// initialize config from consul and set up watchers
-	wp, err := consul.KVPrefixConfigurator(c, prefix)
-	if err != nil {
-		panic("trouble configurating")
-	}
-
-	// ensure that reads from the config are thread safe
-	cc := c.copy() // work with a copy of the configuration
-	fmt.Printf("var1=%s", cc.var1)
-
-	wp.Stop()
-}
-
 func TestConfigurator(t *testing.T) {
 	suite.Run(t, &ConfiguratorTestSuite{})
 }
@@ -70,20 +47,15 @@ type config struct {
 	var2 int
 	service []*api.ServiceEntry
 	t    *testing.T
-	sync.RWMutex
 }
 
 // Update() handles both the initial settings and updates when anything under the prefix changes
-// or when there are changes to any registered services.
+// or when there are changes to any registered services. Changes are made to a private copy of 'c'
+// so thread safety is not an issue.
 func (c *config) Update(_ uint64, data interface{}) {
 
 	var err error
 
-	// Code accessing "config" should grab a read lock or use sync/atomic to achieve thread safety
-	c.Lock()
-	defer c.Unlock()
-
-	// We may consider using Update() for services and other updates
 	switch data.(type) {
 
 	case api.KVPairs:
@@ -94,7 +66,6 @@ func (c *config) Update(_ uint64, data interface{}) {
 			switch kvp.Key {
 			case prefix + key1:
 				c.var1 = string(kvp.Value)
-				c.t.Logf("c.var1=%s", c.var1)
 			case prefix + key2:
 				c.var2, err = strconv.Atoi(string(kvp.Value))
 				if err != nil {
@@ -113,13 +84,6 @@ func (c *config) Update(_ uint64, data interface{}) {
 	}
 }
 
-func (c *config) copy() *config {
-	c.RLock()
-	defer c.RUnlock()
-	cc := *c
-	return &cc
-}
-
 // 1. Test that the consul KV config is transferred correctly
 // 2. Verify that an update is reflected in the config object
 func (cs *ConfiguratorTestSuite) TestKVInit() {
@@ -127,39 +91,44 @@ func (cs *ConfiguratorTestSuite) TestKVInit() {
 
 	cs.buildKVTestData()
 
-	config := &config{
+	c := &config{
 		t: cs.T(),
 	}
+	cm := cs.client.NewConfigManager(c)
 
-	var wp *watch.Plan
-	wp, err = cs.client.KVPrefixConfigurator(config, prefix)
+	err = cm.AddKVPrefix(prefix)
+	require.Nil(cs.T(), err, "AddKVPrefix(%s) failed: %s", prefix, err)
 
-	require.Nil(cs.T(), err, "InitConfigurator(..., %s) failed: %s", prefix, err)
+	time.Sleep(time.Second)
+
+	cs.T().Logf("cm=%+v",cm)
+	c = cm.Read().(*config)
 
 	// Check that config has what we expect
-	require.Equal(cs.T(), val1, config.var1, "the initialized val1 is not what I expected")
-	require.Equal(cs.T(), val2, config.var2, "the initialized val2 is not what I expected")
+	require.Equal(cs.T(), val1, c.var1, "the initialized val1 is not what I expected")
+	require.Equal(cs.T(), val2, c.var2, "the initialized val2 is not what I expected")
 
+	// Change the kv:s in consul
+	cs.T().Log("=== changing the kv values")
+	ch := cm.Subscribe()
 	kv1 := &api.KVPair{Key: prefix + key1, Value: []byte(val1b)}
 	_, err = cs.client.KV().Put(kv1, nil)
 	require.Nil(cs.T(), err, "Trouble changing the value of %s", key1)
-	time.Sleep(time.Second)
-	require.Equal(cs.T(), val1b, config.var1, "var1 is not what i expected after updating in consul")
+	c = (<- *ch).(*config)
+	require.Equal(cs.T(), val1b, c.var1, "var1 is not what i expected after updating in consul")
 
 	kv2 := &api.KVPair{Key: prefix + key2, Value: []byte(fmt.Sprintf("%d", val2b))}
 	_, err = cs.client.KV().Put(kv2, nil)
 	require.Nil(cs.T(), err, "Trouble changing the value of %s", key2)
-	time.Sleep(time.Second)
-	require.Equal(cs.T(), val2b, config.var2, "var2 is not what i expected after updating in consul")
-
-	time.Sleep(time.Second)
+	c = (<- *ch).(*config)
+	require.Equal(cs.T(), val2b, c.var2, "var2 is not what i expected after updating in consul")
 
 	// report what is actually in the kv prefix now:
 	kvps, _, err := cs.client.KV().List(prefix, nil)
-	config.Update(0, kvps)
-	cs.T().Logf("config after manual update: %+v", config)
+	c.Update(0, kvps)
+	cs.T().Logf("config after manual update: %+v", c)
 
-	wp.Stop()
+	cm.Stop()
 }
 
 // 1. Start a web service that provides a health check
@@ -170,19 +139,21 @@ func (cs *ConfiguratorTestSuite) TestServiceInit() {
 
 	cs.buildTestService()
 
-	config := &config{
+	c := &config{
 		t: cs.T(),
 	}
+	cm := cs.client.NewConfigManager(c)
 
-	// Initialize config and start a service watcher
-	wp, err := cs.client.ServiceConfigurator(config, serviceName, serviceTag1, false,nil)
-	require.Nil(cs.T(), err, "Unable to configure the service")
+	err := cm.AddService(serviceName, serviceTag1, false)
+	require.Nil(cs.T(), err, "AddKVPrefix(%s) failed: %s", prefix, err)
 
-	cc := config.copy()
-	require.Equal(cs.T(), 1, len(cc.service), "Expecting exactly one service here")
+	cs.T().Logf("cm=%+v",cm)
+
+	c = cm.Read().(*config)
+	require.Equal(cs.T(), 1, len(c.service), "Expecting exactly one service here")
 
 	// List the health checks before the service can be expected to pass
-	se := cc.service[0]
+	se := c.service[0]
 	for _,check := range se.Checks {
 		cs.T().Logf("check[%s]=%s",check.Name,check.Status)
 	}
@@ -191,16 +162,16 @@ func (cs *ConfiguratorTestSuite) TestServiceInit() {
 	time.Sleep(10*time.Second)
 
 	// The service should be passing now
-	cc = config.copy()
-	require.Equal(cs.T(), 1, len(cc.service), "Expecting exactly one service here")
+	c = cm.Read().(*config)
+	require.Equal(cs.T(), 1, len(c.service), "Expecting exactly one service here")
 
-	se = cc.service[0]
+	se = c.service[0]
 	for _,check := range se.Checks {
 		cs.T().Logf("check[%s]=%s",check.Name,check.Status)
 	}
 
 	// Clean up
-	wp.Stop()
+	cm.Stop()
 }
 
 // buildKVTestData populates a kv path with some data
@@ -266,5 +237,6 @@ func (cs *ConfiguratorTestSuite) TearDownTest() {
 }
 
 func (cs *ConfiguratorTestSuite) TearDownSuite() {
+	cs.T().Log("TearDownSuite()")
 	cs.TearDownTest()
 }
